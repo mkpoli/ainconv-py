6466aa95928554cd8f0ec25c7c8a43302ba6db23
mkpoli <git@mkpo.li>
2025-01-02 03:13:55 +0900

    Merge tests

--- tests/test_syllable.py
+++ tests/test_syllable.py
@@ -2,33 +2,8 @@ import json
 
 from src.ainconv.syllable import separate
 
-SEPARATION_TEST_CASES = {
-    "eyaykosiramsuypa": ["e", "yay", "ko", "si", "ram", "suy", "pa"],
-    "irankarapte": ["i", "ran", "ka", "rap", "te"],
-    "aynu itak": ["ay", "nu", " ", "i", "tak"],
-    "wósekamuy": ["wó", "se", "ka", "muy"],
-    "sineno  yayerampewtekkare": [
-        "si",
-        "ne",
-        "no",
-        "  ",
-        "ya",
-        "ye",
-        "ram",
-        "pew",
-        "tek",
-        "ka",
-        "re",
-    ],
-    "tané": ["ta", "né"],
-    "ônâ": ["ô", "nâ"],
-}
-
 
 def test_separate() -> None:
-    for text, syllables in SEPARATION_TEST_CASES.items():
-        assert separate(text) == syllables
-
     for case in cases:
         assert separate(case["latn"]) == case["syllables"]
 
